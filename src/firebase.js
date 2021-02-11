27312e08986a39dd0fa73c1340fe4c59545e01c6
biplob14 <biploburrahaman@gmail.com>
2021-02-11 13:14:02 +0600

    initialized all files and component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAphBAFNcCkKaEzsqGPTLSaXZrz8Y7ddSA",
+    authDomain: "e-bazar-44250.firebaseapp.com",
+    projectId: "e-bazar-44250",
+    storageBucket: "e-bazar-44250.appspot.com",
+    messagingSenderId: "285552718884",
+    appId: "1:285552718884:web:3d1cb62b4a8130ce55d4b1",
+    measurementId: "G-91XY1CGV92"
+  };
